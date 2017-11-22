bfc2d1cd916d1ae3fc022f5dccc3c78f0c494218
LLNH <m13910396327@163.com>
2017-11-22 17:36:00 +0800

    太阳系

--- pyassign1/planets.py
+++ pyassign1/planets.py
@@ -1,6 +1,5 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import turtle
+import turtle
+import math
 alex = turtle.Turtle()
 bob = turtle.Turtle()
 alice = turtle.Turtle()
@@ -11,32 +10,33 @@ fish = turtle.Turtle()
 
 bob.shape("circle")
 bob.color("yellow")
+bob.pu()
 bob.goto(50,0)
+bob.pd()
 
 t = [alex,alice,may,juice,black,fish]
 colors= ['red','blue','green','black','grey','orange']
-ax = [100,180,300,230,200,400]
-bx = [100,150,350,200,120,300]
+ax = [90,120,200,230,300,400]
+bx = [70,100,170,200,220,300]
 
 for f in range(0,len(t)):
     t[f].shape("circle")
     t[f].color(colors[f])
     t[f].pu()
-    t[f].goto(-ax[f],0)
+    t[f].goto(ax[f],0)
     t[f].pd()
     
 
-
 for u in range(1000):
-    for x in range(-100,101):
+    for x in range(100000):
         for i in range(0,len(t)):
-            currx = x/100*ax[i]
-            y = (bx[i]**2-bx[i]**2/ax[i]**2*currx**2)**(1/2)
+            currx = ax[i]*math.cos(x/(100*(i+1)))
+            y = bx[i]*math.sin(x/(100*(i+1)))
             t[i].goto(currx,y)
-    for m in range(-100,101):
+    for m in range(100000):
         for n in range(0,len(t)):
-            currm = m/100*ax[n]
-            ym = -(bx[n]**2-bx[n]**2/ax[n]**2*currm**2)**(1/2)
+            currm = math.cos(m/(100*(m+1)))*ax[n]
+            ym = -(bx[n]*math.sin(m/(100*(m+1))))
             t[n].goto(-currm,ym)
         
 
